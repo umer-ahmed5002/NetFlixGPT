5526fe43113054cfcbccfcd418dd1088c8df079c
daniyal094 <ahmeddaniyal52@gmail.com>
2024-05-10 14:21:53 +0500

    first commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDXPQl9J_SUeobqh39mtCQb1KhRI-PwxdE",
-  authDomain: "netflifgpt-2c734.firebaseapp.com",
-  projectId: "netflifgpt-2c734",
-  storageBucket: "netflifgpt-2c734.appspot.com",
-  messagingSenderId: "623622957142",
-  appId: "1:623622957142:web:0f86bb7186cbdcfba0b3f7",
-  measurementId: "G-9CRYTE056D"
+  apiKey: "AIzaSyAT3KpF78tkA_qHUebsELNNQrT9pbM6I6w",
+  authDomain: "website-netflix-2de5c.firebaseapp.com",
+  projectId: "website-netflix-2de5c",
+  storageBucket: "website-netflix-2de5c.appspot.com",
+  messagingSenderId: "865624090475",
+  appId: "1:865624090475:web:452226b2640553d4660e2e"
 };
 
 // Initialize Firebase
